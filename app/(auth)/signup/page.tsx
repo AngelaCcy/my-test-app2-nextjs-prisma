418387b7a584eecc877d77f9a4d16caacb139c19
AngelaCcy <angela910914@gmail.com>
2024-12-10 23:12:18 +1000

    fix deployment errors

---
+++ app/(auth)/signup/page.tsx
@@ -0,0 +1,11 @@
+
+
+const SignUpPage = async () => {
+    return (
+        <div className="w-full flex flex-col items-center">
+            Sign Up
+        </div>
+    )
+}
+
+export default SignUpPage
---
+++ components/ui/form/signup-form.tsx
@@ -0,0 +1,11 @@
+
+
+const SignUpForm = async () => {
+    return (
+        <div className="w-full flex flex-col items-center">
+            Sign Up Form
+        </div>
+    )
+}
+
+export default SignUpForm
