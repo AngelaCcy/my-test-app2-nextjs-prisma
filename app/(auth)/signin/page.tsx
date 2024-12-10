2784b5d741e9ad95522777b617855232e611f8d0
AngelaCcy <angela910914@gmail.com>
2024-12-10 23:08:23 +1000

    fix deployment errors

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -4,15 +4,7 @@ import { auth } from "@/auth";
 import Image from "next/image";
 import logo from "@/public/img/logo.png";
 
-interface SignInPageProps {
-    searchParams: {
-        callbackUrl: string
-    }
-}
-
-const SignInPage = async ({
-    searchParams: { callbackUrl }
-}: SignInPageProps) => {
+const SignInPage = async () => {
     const session = await auth();
     if (session?.user) {
         return redirect("/");
@@ -21,7 +13,7 @@ const SignInPage = async ({
         <div className="w-full flex flex-col items-center">
             <Image src={logo} alt="menu" width={150} height={150}/>
             <h1 className="text-2xl font-bold">Sign In to Your Account</h1>
-            <SignInForm callbackUrl={callbackUrl || "/"} />
+            <SignInForm />
         </div>
     )
 }
